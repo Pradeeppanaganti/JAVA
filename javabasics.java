db75445a05b5448e9ade838c58cb03e16cbaca5a
Panaganti-Pradeep <pradeep03102005k@gmail.com>
2025-09-27 14:05:46 +0530

    Day 1 of Java

---
+++ javabasics.java
@@ -0,0 +1,82 @@
+public class javabasics {
+    public static void main(String args[]) {
+        /* byte a = 5;
+        System.out.println(a);
+        char ch = 'A';
+        System.out.println(ch);
+        boolean b = true;
+        System.out.println(b);
+        float f = 5.5f;
+        System.out.println(f);
+        int number = 25;
+        //long
+        //double
+        short n = 56;
+        System.out.println(n);
+         */ // code to calculate sum of two numbers
+        /* This
+        is
+        a multi line
+        comment
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println("The sum is " + sum);
+         */
+        //Scanner sc = new Scanner(System.in);
+        //String input = sc.next();
+        //System.out.println(input);
+        //String name = sc.nextLine();
+        //System.out.println(name);
+        //int number = sc.nextInt();
+        //System.out.println(number);
+        //float marks = sc.nextFloat();
+        //System.out.println(marks);
+        //boolean value = sc.nextboolean();
+        //double value1 = sc.nextDouble();
+        /*long value2 =  sc.nextLong();
+        System.out.println(value2);
+        sc.close();
+         */
+        /* // Sum of a & b
+                Scanner sc = new Scanner(System.in)) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int sum = a + b;
+            System.out.println("THE sum is " + sum);
+        } */
+       // Product of a & b
+        /* Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int product = a * b;
+        System.out.println("The product is " + product); */
+        // Area of circle
+        /* Scanner sc = new Scanner(System.in);
+        float radius = sc.nextFloat();
+        float area = 3.14f * radius * radius;
+        System.out.println("The area of circle is " + area);
+        sc.close(); */
+        /* int a = 25;
+        long b = a;
+        System.out.println(b); */
+        // float a = 25.24718274901327491328f;
+        /* int b = (int) a; // error
+        System.out.println(b); */
+        /* char ch = 'a' ;
+        char ch2 = 'b';
+        int number = ch;
+        int number2 = ch2;
+        System.out.println(number);
+        System.out.println(number2); */
+        /* int a = 10;
+        float b = 20.25f;
+        long c = 30;
+        double d = 212;
+         ans = a + b + c + d;
+        System.out.println(ans); */
+        /* byte a = 5;
+        byte b = (byte) (a * 2);
+        System.out.println(b); */
+    } 
+}
